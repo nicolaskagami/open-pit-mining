59e7ecdea2a570cfdf88b3dc2146987220b2e485
Nicolassk <nicolaskagami@gmail.com>
2015-10-20 09:37:44 -0200

    Script updated

--- runTests.sh
+++ runTests.sh
@@ -9,4 +9,5 @@ do
     TEST="${RESULTS}/$(basename ${file} .ins)"
     ${EXEC} < ${file} > ${TEST}.sol
     ./OPM/OPM --ins $file --sol ${TEST}.sol --pbm ${TEST}.pbm
+    convert ${TEST}.pbm ${TEST}.png
 done
